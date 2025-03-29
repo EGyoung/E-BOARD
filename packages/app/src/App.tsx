7cf9dcb7be52a6748b32598d8b47aa4155b23199
yijuyang <yijuyang@cvte.com>
2025-03-29 22:03:01 +0800

    feat: 添加样式

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,29 +1,23 @@
 import React, { useEffect } from "react";
-import { Button } from "@e-board/ui";
 import { EBoard } from "@e-board/core";
-let isInit = false;
+import "./styles.css";
+
 const App: React.FC = () => {
   useEffect(() => {
-    console.log("App useEffect");
-    // if (!isInit) {
-    // isInit = true;
     const board = new EBoard({
       container: document.getElementById("board") as HTMLDivElement,
       id: "app-board",
     });
-    (window as any).board = board;
-    // board.init();
+
     return () => {
       board.dispose();
     };
-    // }
   }, []);
+
   return (
-    <>
-      <div id="board" />
-      {/* <h1>Welcome to E-Board</h1> */}
-      {/* <Button>按钮</Button> */}
-    </>
+    <div className="app-container">
+      <div id="board" className="board-container" />
+    </div>
   );
 };
 
