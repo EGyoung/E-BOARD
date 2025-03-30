78dad822a672486449989a964cbbc3abb1c7d0bc
yijuyang <yijuyang@cvte.com>
2025-03-30 14:40:44 +0800

    fix: eslint 和 prettier 冲突

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,26 +7,39 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
+    "prettier"
   ],
   env: {
     browser: true,
     node: true,
-    es6: true,
+    es6: true
   },
   settings: {
     react: {
-      version: "detect",
-    },
+      version: "detect"
+    }
   },
   rules: {
-    "prettier/prettier": "error",
+    "prettier/prettier": [
+      "error",
+      {
+        semi: true,
+        singleQuote: false,
+        trailingComma: "none",
+        printWidth: 100,
+        tabWidth: 2,
+        useTabs: false,
+        bracketSpacing: true,
+        arrowParens: "avoid",
+        endOfLine: "lf"
+      }
+    ],
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "warn",
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "no-console": ["warn", { allow: ["warn", "error"] }],
     "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
+    "react/prop-types": "off"
   },
-  ignorePatterns: ["dist", "node_modules", "*.js", "!.eslintrc.js"],
+  ignorePatterns: ["dist", "node_modules", "*.js", "!.eslintrc.js"]
 };
