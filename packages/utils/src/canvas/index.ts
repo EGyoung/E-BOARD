a0f19f928a4c93b7b679f057768d6835b8834ade
Juyoung <juyoung@JuyoungdeMacBook-Pro.local>
2025-08-26 23:20:00 +0800

    fix: 无法书写

--- packages/utils/src/canvas/index.ts
+++ packages/utils/src/canvas/index.ts
@@ -12,19 +12,7 @@ export function initContextAttrs(
 ) {
   const zoom = options?.zoom || 1;
   const currentConfig = config || {};
-  if (currentConfig.lineWidth !== undefined)
-    currentConfig.lineWidth = Math.max(1, currentConfig.lineWidth * zoom);
-  console.log(currentConfig, "currentConfig");
-
   merge(context, currentConfig);
-  // 设置绘制样式
-  // context.lineCap = "round"; // 设置线条端点样式
-  // context.lineJoin = "round"; // 设置线条连接处样式
-  // context.strokeStyle = "white"; // 设置线条颜色
-  // // 根据缩放比例调整线条宽度，保持视觉一致性，但设置最小宽度避免线条过细
-  // context.lineWidth = Math.max(1, 4 * zoom);
-  // context.globalCompositeOperation = "source-over";
-  // context.globalAlpha = 1.0;
-  // context.imageSmoothingEnabled = true;
-  // context.imageSmoothingQuality = "high";
+  if (currentConfig.lineWidth !== undefined)
+    context.lineWidth = Math.max(1, currentConfig.lineWidth * zoom);
 }
--- packages/utils/src/merge/index.ts
+++ packages/utils/src/merge/index.ts
@@ -1,6 +1,6 @@
 // 对象合并函数 不是undefined就覆盖
 const merge = <T>(target: T, source: Partial<T>): T => {
-  const result = { ...target };
+  const result = target;
   for (const key in source) {
     if (source[key] !== undefined) {
       result[key] = source[key] as T[Extract<keyof T, string>];
