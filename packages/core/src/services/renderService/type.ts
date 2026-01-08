99c0a0a1899a263f356fed973c7270e35dae123b
yijuyang <yijuyang@cvte.com>
2026-01-08 16:36:07 +0800

    feat: 抽离tileManager

--- packages/core/src/services/renderService/type.ts
+++ packages/core/src/services/renderService/type.ts
@@ -2,3 +2,13 @@ import type RenderService from "./index";
 export type IRenderService = RenderService;
 
 export const IRenderService = Symbol("IRenderService");
+export type Range = {
+    minX: number,
+    minY: number,
+    maxX: number,
+    maxY: number
+}
+
+export interface IDrawModelHandler {
+    (model: any, ctx?: CanvasRenderingContext2D): void;
+}
